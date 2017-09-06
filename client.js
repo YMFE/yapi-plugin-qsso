ce03b523c9c7ee6b1908b62d9e74fb6f0c268ab8
sean <wenxiong.su@qunar.com>
2017-09-06 10:07:57 +0800

    test: 删除测试代码

--- client.js
+++ client.js
@@ -1,12 +1,10 @@
 import React from 'react';
 const Qsso = require('./qsso-lib');
-const diff = require('fast-diff');
-console.log(diff);
+
 class QssoComponent extends React.Component{
   componentDidMount(){
     Qsso.attach('qsso-login', '/api/user/login_by_token');
   }
-
   render(){
     return <button id="qsso-login"   className="btn-home btn-home-normal" >QSSO 登录</button>;
   }
