32fdc0aa3f9894eafee137e4c90dd7e0485e64fa
ariesly <ileeyi@qq.com>
2022-05-05 09:28:10 +0800

    redirect next

--- client.js
+++ client.js
@@ -1,36 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react';
 
 const qualifyURL = (url, encode) => {
-  url = url || '';
-  var ret = location.protocol + '//' + location.host + (url.substr(0, 1) === '/' ? '' : location.pathname.match(/.*\//)) + url;
-  if (encode) {
-    ret = encodeURIComponent(ret);
-  }
-  return ret;
-}
-
-module.exports = function (options) {
-  const handleLogin = () => {
-    const loginURI = '/api/user/login_by_token';
-    const { AUTH_SERVER } = options;
-    let ret = qualifyURL(loginURI, true);
-    let redirectURL = AUTH_SERVER  + '?ret=' + ret;
-    location.href = redirectURL;
-  }
-
-  const QssoComponent = () => (
-    <button onClick={handleLogin} className="btn-home btn-home-normal" >QSSO 登录</button>
-  )
-
-  this.bindHook('third_login', QssoComponent);
+    url = url || '';
+    var ret =
+        location.protocol +
+        '//' +
+        location.host +
+        (url.substr(0, 1) === '/' ? '' : location.pathname.match(/.*\//)) +
+        url;
+    if (encode) {
+        ret = encodeURIComponent(ret);
+    }
+    return ret;
 };
 
+module.exports = function (options) {
+    const handleLogin = (props = {}) => {
+        const next = props.state && props.state.next;
+        const ext = next ? '&ext=' + encodeURIComponent(URLStringify({ next })) : '';
 
+        const loginURI = '/api/user/login_by_token';
+        const { AUTH_SERVER } = options;
+        let ret = qualifyURL(loginURI, true);
+        let redirectURL = AUTH_SERVER + '?ret=' + ret + ext;
+        location.href = redirectURL;
+    };
 
+    const QssoComponent = (props = {}) => (
+        <button onClick={() => handleLogin(props)} className='btn-home btn-home-normal'>
+            QSSO 登录
+        </button>
+    );
 
+    this.bindHook('third_login', QssoComponent);
+};
 
-
-
-
-
-
+function URLStringify(o) {
+    var ret = [];
+    for (var i in o) {
+        ret.push(i + '=' + o[i]);
+    }
+    return ret.join('&');
+}
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const request = require('request');
 
 module.exports = function (options) {
-  const { loginUrl, emailPostfix } = options;
+    const { loginUrl, emailPostfix } = options;
 
-  this.bindHook('third_login', (ctx) => {
-    let token = ctx.request.body.token || ctx.request.query.token;
-    return new Promise((resolve, reject) => {
-      request(loginUrl + token, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          let result = JSON.parse(body);
-          if (result && result.ret === true) {
-            let ret = {
-              email: result.userId + emailPostfix,
-              username: result.data.userInfo.name
-            };
-            resolve(ret);
-          } else {
-            reject(result);
-          }
-        }
-        reject(error);
-      });
+    this.bindHook('third_login', (ctx) => {
+        let token = ctx.request.body.token || ctx.request.query.token;
+        return new Promise((resolve, reject) => {
+            request(loginUrl + token, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    let result = JSON.parse(body);
+                    if (result && result.ret === true) {
+                        let ret = {
+                            email: result.userId + emailPostfix,
+                            username: result.data.userInfo.name
+                        };
+                        const next = ctx.request.body.next;
+                        if (next) {
+                            ret.next = next;
+                        }
+                        resolve(ret);
+                    } else {
+                        reject(result);
+                    }
+                }
+                reject(error);
+            });
+        });
     });
-  })
-}
+};
